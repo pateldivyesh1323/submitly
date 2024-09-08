11ff54b9668727c5861dfb4bb551cd566ea22913
Patel Divyesh <pateldivyesh1323@gmail.com>
2024-09-08 12:12:33 +0530

    Added hero animation

--- packages/client/src/pages/Home.tsx
+++ packages/client/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import { Flex, Text } from "@radix-ui/themes";
 
 export default function Home() {
@@ -5,24 +6,37 @@ export default function Home() {
     <Flex
       align="center"
       justify="center"
-      direction="column"
-      className="min-h-[80vh] w-[80vw] mx-auto"
+      className="relative min-h-[80vh] w-[80vw] mx-auto"
     >
-      <Text color="lime" size="4" className="font-guerrilla">
-        Justification
-      </Text>
-      <Text size="9" className="font-serif">
-        Contact forms made easier
-      </Text>
-      <Text color="gray" align="center">
-        Submitly is a seamless form submission solution that simplifies how
-        users interact with your website. Without the need for a backend server,
-        Submitly allows you to receive form submissions directly into your
-        inbox. It offers an intuitive and user-friendly experience, helping you
-        streamline communication and data collection for any online project.
-        Perfect for landing pages, portfolio sites, or businesses that want fast
-        and efficient form management without complicated server setups.
-      </Text>
+      <DotLottieReact
+        src="/assets/animations/HeroAnimation.json"
+        loop
+        autoplay
+        className="absolute h-[70%] w-[70%] object-cover z-0 opacity-15"
+      />
+      <Flex
+        align="center"
+        justify="center"
+        direction="column"
+        className="relative z-10"
+      >
+        <Text color="lime" size="4" className="font-guerrilla">
+          Justification
+        </Text>
+        <Text size="9" className="font-serif">
+          Contact forms made easier
+        </Text>
+        <Text color="gray" align="center">
+          Submitly is a seamless form submission solution that simplifies how
+          users interact with your website. Without the need for a backend
+          server, Submitly allows you to receive form submissions directly into
+          your inbox. It offers an intuitive and user-friendly experience,
+          helping you streamline communication and data collection for any
+          online project. Perfect for landing pages, portfolio sites, or
+          businesses that want fast and efficient form management without
+          complicated server setups.
+        </Text>
+      </Flex>
     </Flex>
   );
 }
