efb6b8d818eee8f00eb8c45f6b5bd4895ee09040
Patel Divyesh <pateldivyesh1323@gmail.com>
2024-09-10 01:54:24 +0530

    Fixed CORS error

--- packages/server/src/environments.ts
+++ packages/server/src/environments.ts
@@ -6,4 +6,5 @@ export default {
   PORT: process.env.PORT || 8000,
   JWT_SECRET: process.env.JWT_SECRET,
   MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/submitly",
+  ORIGIN_URI: process.env.ORIGIN_URI || "http://localhost:5173",
 };
