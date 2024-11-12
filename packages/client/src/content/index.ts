c47762ba36dc957d3f701ebdbd29f8484f4284fd
Patel Divyesh <pateldivyesh1323@gmail.com>
2024-11-12 16:22:59 +0530

    Added features section

---
+++ packages/client/src/content/index.ts
@@ -0,0 +1,38 @@
+export const featuresContent = [
+  {
+    icon: "/assets/icons/features/accept.png",
+    title: "Seamless Integration",
+    description:
+      "Effortlessly integrate with your existing tools and workflows, with the help of our easy-to-use API.",
+  },
+  {
+    icon: "/assets/icons/features/database.png",
+    title: "No Backend Required",
+    description:
+      "Submitly is a serverless solution, meaning you don't need to worry about setting up a backend server.",
+  },
+  {
+    icon: "/assets/icons/features/friends.png",
+    title: "User-Friendly Interface",
+    description:
+      "Our intuitive and user-friendly interface makes it easy for you to manage form submissions.",
+  },
+  {
+    icon: "/assets/icons/features/flexible.png",
+    title: "Customizable Forms",
+    description:
+      "Form fields are fully customizable, allowing you to collect the data that matters most to you.",
+  },
+  {
+    icon: "/assets/icons/features/secure.png",
+    title: "Secure Submissions",
+    description:
+      "We take security seriously, with built-in measures to protect your data and form submissions.",
+  },
+  {
+    icon: "/assets/icons/features/data-analytics.png",
+    title: "Analytics & Reporting",
+    description:
+      "Gain insights into your form submissions with detailed analytics and reporting tools.",
+  },
+];
--- packages/client/src/pages/Home.tsx
+++ packages/client/src/pages/Home.tsx
@@ -1,52 +1,81 @@
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
-import { Flex, Text } from "@radix-ui/themes";
+import { Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
+import { featuresContent } from "../content";
 
 export default function Home() {
   return (
-    <Flex
-      align="center"
-      justify="center"
-      className="relative min-h-[80vh] w-[80vw] mx-auto"
-    >
-      <DotLottieReact
-        src="/assets/animations/HeroAnimation.json"
-        loop
-        autoplay
-        className="absolute h-[70%] w-[70%] object-cover z-0 opacity-15"
-      />
+    <Flex align="center" justify="center" direction="column">
       <Flex
         align="center"
         justify="center"
-        direction="column"
-        className="relative z-10"
+        className="relative min-h-[100vh] w-[80vw] mx-auto"
       >
-        <Text className="font-serif lg:text-6xl md:text-4xl text-2xl">
-          Contact forms made easier
-        </Text>
-        <Text
-          color="gray"
+        <DotLottieReact
+          src="/assets/animations/HeroAnimation.json"
+          loop
+          autoplay
+          className="absolute h-[70%] w-[70%] object-cover z-0 opacity-15"
+        />
+        <Flex
           align="center"
-          className="text-justify text-sm lg:text-xl"
+          justify="center"
+          direction="column"
+          className="relative"
         >
-          Submitly is a seamless form submission solution that simplifies how
-          users interact with your website. Without the need for a backend
-          server, Submitly allows you to receive form submissions directly into
-          your inbox. It offers an intuitive and user-friendly experience,
-          helping you streamline communication and data collection for any
-          online project. Perfect for landing pages, portfolio sites, or
-          businesses that want fast and efficient form management without
-          complicated server setups.
-        </Text>
-        <Link to="/docs">
-          <Flex align="center" className="gap-1 hover:gap-4 transition-all">
-            <Text color="lime">Go to Documentation</Text>
-            <Text color="lime" size="7">
-              &rarr;
-            </Text>
-          </Flex>
-        </Link>
+          <Text className="font-serif lg:text-6xl md:text-4xl text-2xl">
+            Contact forms made easier
+          </Text>
+          <Text
+            color="gray"
+            align="center"
+            className="text-justify text-sm lg:text-xl"
+          >
+            Submitly is a seamless form submission solution that simplifies how
+            users interact with your website. Without the need for a backend
+            server, Submitly allows you to receive form submissions directly
+            into your inbox. It offers an intuitive and user-friendly
+            experience, helping you streamline communication and data collection
+            for any online project. Perfect for landing pages, portfolio sites,
+            or businesses that want fast and efficient form management without
+            complicated server setups.
+          </Text>
+          <Link to="/docs">
+            <Flex align="center" className="gap-1 hover:gap-4 transition-all">
+              <Text color="lime">Go to Documentation</Text>
+              <Text color="lime" size="7">
+                &rarr;
+              </Text>
+            </Flex>
+          </Link>
+        </Flex>
       </Flex>
+      <Flex
+        direction="column"
+        justify="center"
+        align="center"
+        className="w-[80vw]"
+        gap="4"
+      >
+        <Heading as="h3">Features</Heading>
+        <Grid columns="2" className="p-4" gap="8">
+          {featuresContent.map((feature, index) => (
+            <Flex
+              key={index}
+              direction="column"
+              className="bg-neutral-800 rounded-lg p-4 gap-4"
+            >
+              <div>
+                <img src={feature.icon} alt="" className="w-[40px] h-[40px]" />
+              </div>
+              <div className="font-bold">{feature.title}</div>
+              <div className="text-sm">{feature.description}</div>
+            </Flex>
+          ))}
+        </Grid>
+      </Flex>
+      <Footer />
     </Flex>
   );
 }
